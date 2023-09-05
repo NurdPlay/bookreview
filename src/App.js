0f9556e58b6745f1a8f69630fb7aa880739f4245
John Elliott <j.elliott1700@gmail.com>
2023-09-05 13:56:05 -0700

    added a navbar with a logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import BookLogo from './app/assets/img/logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      *Book Review*
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={BookLogo} alt='book logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
     </div>
   );
 }
